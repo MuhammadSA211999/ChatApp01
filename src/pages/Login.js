f9bbb277b4dcbe49a4f0636deddc452c22c072eb
Muhammad SA <MuhammadSA211999>
2023-03-17 01:46:42 +0600

    loggin and auth checked

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -95,13 +95,13 @@ export default function Login() {
                         </div>
 
                         <div>
-                            {/* <button
+                            <button
                                 disabled={isLoading}
                                 type="submit"
                                 className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-violet-600 hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-violet-500"
                             >
                                 Sign in
-                            </button> */}
+                            </button>
                         </div>
 
                         {error !== '' && <p className="text-red-500">{error}</p>}
