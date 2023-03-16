9fa76ed1bc389bb7ceb75b7ebe1d1c69f8d32d8e
Muhammad SA <MuhammadSA211999>
2023-03-17 01:46:01 +0600

    login

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -95,8 +95,8 @@ export default function Register() {
                                     type="email"
                                     autoComplete="email"
                                     required
-                                // className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm"
-                                // placeholder="Email address"
+                                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm"
+                                    placeholder="Email address"
                                 />
                             </div>
 
