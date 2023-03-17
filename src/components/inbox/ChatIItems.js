7d0f101354f89b1f6ea1c10e795547bbc6ad54c5
Muhammad SA <MuhammadSA211999>
2023-03-17 21:48:48 +0600

    get conversations

--- src/components/inbox/ChatIItems.js
+++ src/components/inbox/ChatIItems.js
@@ -1,28 +1,47 @@
+import { useSelector } from "react-redux";
+import { useGetConversationsQuery } from "../../features/conversations/conversationsApi";
+import getPartnerInfo from "../../utils/getPartnerInfo";
+import Error from "../ui/Error";
 import ChatItem from "./ChatItem";
+import moment from 'moment';
+import gravatarUrl from "gravatar-url";
 
 export default function ChatItems() {
-    return (
-        <ul>
-            <li>
+    const { auth } = useSelector((state) => state.auth) || {}
+    //loggedin user email from redux store
+    const { email } = auth || {}
+
+    //get all conversations info 
+    const { data: conversations, isLoading, isError, error } = useGetConversationsQuery(email)
+
+    // decide what to render 
+    let content = null
+    if (isLoading) {
+        content = <li>Loading conversations...</li>
+    }
+    else if (!isLoading && isError) {
+        content = <li><Error message={error} /></li>
+    }
+    else if (!isLoading && !isError && conversations.length === 0) {
+        content = <li className='m-t'>No conversations found</li>
+    }
+    else if (!isLoading && !isError && conversations.length > 0) {
+        content = conversations.map(conversation => {
+            const { id, message, timestamp } = conversation
+            const { name, email: partnerEmail } = getPartnerInfo(conversation.users, email)
+
+            return <li key={id}>
                 <ChatItem
-                    avatar="https://cdn.pixabay.com/photo/2018/09/12/12/14/man-3672010__340.jpg"
-                    name="Saad Hasan"
-                    lastMessage="bye"
-                    lastTime="25 minutes"
-                />
-                <ChatItem
-                    avatar="https://cdn.pixabay.com/photo/2018/09/12/12/14/man-3672010__340.jpg"
-                    name="Sumit Saha"
-                    lastMessage="will talk to you later"
-                    lastTime="10 minutes"
-                />
-                <ChatItem
-                    avatar="https://cdn.pixabay.com/photo/2018/09/12/12/14/man-3672010__340.jpg"
-                    name="Mehedi Hasan"
-                    lastMessage="thanks for your support"
-                    lastTime="15 minutes"
+                    avatar={gravatarUrl(partnerEmail, { size: 80 })}
+                    name={name}
+                    lastMessage={message}
+                    lastTime={moment(timestamp).fromNow()}
                 />
             </li>
-        </ul>
+        })
+    }
+
+    return (
+        <ul>{content}</ul>
     );
 }
